25938b5bf45dd68d41ac2da39e75e0d88ae45f6d
uidoGG <guidobaragatti@hotmail.com>
2023-01-14 19:57:47 +0100

    headeryfooter

---
+++ src/components/Footer.js
@@ -0,0 +1,26 @@
+import React  from "react"; 
+import {SiLinkedin} from "react-icons/si"
+
+
+const Footer = () => {
+
+
+    return (<footer>
+
+<a
+          className="App-link"
+          href="https://www.linkedin.com/in/guido-baragatti/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Linkedin &nbsp;
+          <SiLinkedin/>
+        </a>
+
+
+    </footer>)
+
+}
+
+export default Footer;
+
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React  from "react"; 
+
+const Header = () => {
+    return ( <header className="App-header">
+    <p>
+      Editame esta
+    </p>
+    
+  </header>)
+
+}
+
+export default Header;
+
