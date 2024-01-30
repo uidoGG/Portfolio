df1ea429901e070707ae9f4a1f62578852617c16
uidogg <guidobaragatti@hotmail.com>
2024-01-30 16:19:26 +0100

    new features

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const navbarOffsetTop = navbar.offsetTop;
 const sections = document.querySelectorAll("section");
 const navbarLinks = document.querySelectorAll(".navbar-link");
 const progress = document.querySelector(".progress-bars-wrapper");
-const progressBarPercents = [90, 90, 72, 70, 60];
+const progressBarPercents = [90, 90, 90, 70, 40];
 
 window.addEventListener("scroll", () => {
   mainFn();
@@ -41,64 +41,45 @@ window.addEventListener("resize", () => {
   window.location.reload();
 });
 
+var _CONTENT = ["Front-end developer", "Junior Full-Stack"];
 
-/* Typing and Deleting Effect */
-// List of sentences
-var _CONTENT = [ 
-	"FRONT-END DEVELOPER", 
-	"WEB DEVELOPER"
-];
+var _PART = 0;
+var _PART_INDEX = 0;
+var _INTERVAL_VAL;
+var _ELEMENT = document.querySelector("#text-name");
+var _CURSOR = document.querySelector("#cursor");
 
-var _PART = 0; // Current sentence being processed
-var _PART_INDEX = 0; // Character number of the current sentence being processed 
-var _INTERVAL_VAL; // Holds the handle returned from setInterval
-var _ELEMENT = document.querySelector("#text-name"); // Element that holds the text
-var _CURSOR = document.querySelector("#cursor"); // Cursor element 
+function Type() {
+  var text = _CONTENT[_PART].substring(0, _PART_INDEX + 1);
+  _ELEMENT.innerHTML = text;
+  _PART_INDEX++;
 
-// Implements typing effect
-function Type() { 
-	// Get substring with 1 characater added
-	var text =  _CONTENT[_PART].substring(0, _PART_INDEX + 1);
-	_ELEMENT.innerHTML = text;
-	_PART_INDEX++;
+  if (text === _CONTENT[_PART]) {
+    _CURSOR.style.display = "none";
 
-	// If full sentence has been displayed then start to delete the sentence after some time
-	if(text === _CONTENT[_PART]) {
-		// Hide the cursor
-		_CURSOR.style.display = 'none';
-
-		clearInterval(_INTERVAL_VAL);
-		setTimeout(function() {
-			_INTERVAL_VAL = setInterval(Delete, 50);
-		}, 1000);
-	}
+    clearInterval(_INTERVAL_VAL);
+    setTimeout(function () {
+      _INTERVAL_VAL = setInterval(Delete, 50);
+    }, 1000);
+  }
 }
 
-// Implements deleting effect
 function Delete() {
-	// Get substring with 1 characater deleted
-	var text =  _CONTENT[_PART].substring(0, _PART_INDEX - 1);
-	_ELEMENT.innerHTML = text;
-	_PART_INDEX--;
+  var text = _CONTENT[_PART].substring(0, _PART_INDEX - 1);
+  _ELEMENT.innerHTML = text;
+  _PART_INDEX--;
 
-	// If sentence has been deleted then start to display the next sentence
-	if(text === '') {
-		clearInterval(_INTERVAL_VAL);
+  if (text === "") {
+    clearInterval(_INTERVAL_VAL);
 
-		// If current sentence was last then display the first one, else move to the next
-		if(_PART == (_CONTENT.length - 1))
-			_PART = 0;
-		else
-			_PART++;
-		_PART_INDEX = 0;
+    if (_PART == _CONTENT.length - 1) _PART = 0;
+    else _PART++;
+    _PART_INDEX = 0;
 
-		// Start to display the next sentence after some time
-		setTimeout(function() {
-			_CURSOR.style.display = 'inline-block';
-			_INTERVAL_VAL = setInterval(Type, 100);
-		}, 200);
-	}
+    setTimeout(function () {
+      _CURSOR.style.display = "inline-block";
+      _INTERVAL_VAL = setInterval(Type, 100);
+    }, 200);
+  }
 }
-// Start the typing effect on load
 _INTERVAL_VAL = setInterval(Type, 100);
-
